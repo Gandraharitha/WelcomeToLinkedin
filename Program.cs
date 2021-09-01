92b6b83d9f357f29588c1ca479defc09f450769d
gandr <gandr@LAPTOP-N38BPIT2>
2021-09-01 16:01:55 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WelcomeToLinkedin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
